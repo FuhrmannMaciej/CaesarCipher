6146183f8f3e8716c54893b320f6cc461d5715f0
maciej.fuhrmann <maciej.fuhrmann@happyteam.io>
2021-10-19 16:53:41 +0200

    Added text files to hold dictionary and answers after brute force attack on Caesar Cipher. Implemented brute force attack method.

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 
 namespace CaesarCipher
 {
     class Program
     {
+        private const string answersPath = @"C:\Users\fuhrm\source\repos\CaesarCipher\CaesarCipher\answers.txt";
+        private const string dictionaryPath = @"C:\Users\fuhrm\source\repos\CaesarCipher\CaesarCipher\dictionary.txt";
         private static string alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~";
         private static int alphabetSize = alphabet.Length;
 
-        static int mod(int x, int m)
+        static int Mod(int x, int m)
         {
             int r = x % m;
             return r < 0 ? r + m : r;
@@ -24,10 +28,50 @@ namespace CaesarCipher
                     continue;
                 }
                 char offset = '!';
-                symbol = (char)(mod(((symbol + key) - offset), alphabetSize) + offset);
+                symbol = (char)(Mod(((symbol + key) - offset), alphabetSize) + offset);
                 array[i] = symbol;
             }
             return new string(array);
+        }
+
+        private static ArrayList FileReader ()
+        {
+            ArrayList allDictionaryWords = new ArrayList();
+            foreach (string word in File.ReadLines(dictionaryPath))
+            {
+                allDictionaryWords.Add(word);
+            }
+            return allDictionaryWords;
+        }
+
+        private static void BruteForce(string text)
+        {
+            Console.WriteLine("Started brute-force");
+            ArrayList dictionary = FileReader();
+            StreamWriter sw = File.AppendText(answersPath);
+            for (int i = 1; i <= alphabetSize; i++)
+            {
+                string textToSplit = CaesarCipher(text, i);
+                string[] words = textToSplit.Split(" ");
+                sw.WriteLine("Key: " + i + " . Looping through possible words.");
+                for (int j = 0; j < dictionary.Count; j++)
+                {
+                    foreach (var word in words)
+                    {
+                        if (word.ToLower() == (string)dictionary[j])
+                        {
+                            sw.WriteLine(Array.IndexOf(words, word) + " " + word);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Finished brute-force");
+            sw.Close();
+        }
+
+        private static void ClearAnswersFile ()
+        {
+            File.WriteAllText(answersPath, string.Empty);
         }
 
         static void Main(string[] args)
@@ -50,6 +94,9 @@ namespace CaesarCipher
             string e = CaesarCipher(d, -25); // Ok
             Console.WriteLine(e);
 
+            ClearAnswersFile();
+
+            BruteForce("The only thing we have to fear is fear itself");
 
         }
     }
